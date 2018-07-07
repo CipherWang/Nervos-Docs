7c3ca040e967736db1d8c2193ddcf6672bc2c2f0
Haichao <haichaozhu77@gmail.com>
2018-07-07 11:55:10 +0800

    updates settings

--- script/customization-setting.js
+++ script/customization-setting.js
@@ -6,5 +6,7 @@ var customization = {
 
     name: 'Nervos Network', // name of this document
 
-    homepage: `${default_language}/nervos.md`,// the home page, extension name '.md' cannot be omitted.
+    homepage: 'nervos.md',// the home page, extension name '.md' cannot be omitted.
+
+    
 }
